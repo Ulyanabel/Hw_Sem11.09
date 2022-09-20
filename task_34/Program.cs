18e916e21a5ee44020ecf06168b6b7fa3296ee8e
ulyana_belousova <tentakly@list.ru>
2022-09-20 20:12:29 +0300

    task 34

---
+++ task_34/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве
+int evenNumbers = 0;
+int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    if (array[i] % 2 == 0) evenNumbers++;
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine("Четных числе " + evenNumbers);
